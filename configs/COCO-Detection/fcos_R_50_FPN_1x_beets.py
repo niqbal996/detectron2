14658dc6075dca62010101b033dce8a65d56c11a
Naeem Iqbal <naeemiqbal996@gmail.com>
2023-06-15 13:54:06 +0200

    custom model configs

--- configs/COCO-Detection/fcos_R_50_FPN_1x_beets.py
+++ configs/COCO-Detection/fcos_R_50_FPN_1x_beets.py
@@ -1,19 +1,13 @@
 from ..common.optim import SGD as optimizer
 from ..common.coco_schedule import lr_multiplier_1x as lr_multiplier
 # from ..common.data.coco import dataloader
-from ..common.data.maize import dataloader
+from ..common.data.beets import dataloader
 from ..common.models.fcos import model
 from ..common.train import train
-# from detectron2.config import LazyCall as L
-# solver = L()
+
 dataloader.train.mapper.use_instance_mask = False
 optimizer.lr = 0.01
 
 model.backbone.bottom_up.freeze_at = 2
-model.num_classes = 2
-dataloader.train.total_batch_size = 4
-
-
-
-train.init_checkpoint = "/media/niqbal/T7/trainers/gil_paper/fcos_R_1600_valid_500/model_final.pth"
-# print('hold')
+train.init_checkpoint = "detectron2://ImageNetPretrained/MSRA/R-50.pkl"
+print('hold')
