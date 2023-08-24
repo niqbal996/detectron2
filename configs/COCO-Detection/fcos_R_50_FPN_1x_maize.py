ed6bde3d3e1657c0ee4eeafddc3d7aa3dd7bc632
Naeem Iqbal <naeemiqbal996@gmail.com>
2023-08-24 09:04:50 +0200

    Cleanup and more logging options

--- configs/COCO-Detection/fcos_R_50_FPN_1x_maize.py
+++ configs/COCO-Detection/fcos_R_50_FPN_1x_maize.py
@@ -9,6 +9,7 @@ from ..common.train import train
 
 dataloader.train.mapper.use_instance_mask = False
 optimizer.lr = 1e-4
+optimizer.lr = 1e-4
 
 model.backbone.bottom_up.freeze_at = 2
 model.num_classes = 2
@@ -22,12 +23,21 @@ dataloader.train.total_batch_size = 28  # 28 on 40GB and 80 on 80 GB
 dataloader.train.num_workers = 28
 dataloader.test.batch_size = 20
 dataloader.test.num_workers = 20
+dataloader.train.total_batch_size = 28  # 28 on 40GB and 80 on 80 GB
+dataloader.train.num_workers = 28
+dataloader.test.batch_size = 20
+dataloader.test.num_workers = 20
 
 train.output_dir = "/netscratch/naeem/fcos_pretrained_not_frozen_transformed_1e-4"
 train.init_checkpoint = "detectron2://ImageNetPretrained/MSRA/R-50.pkl"
 train.max_iter = 15000
 train.eval_period = 200
+train.output_dir = "/netscratch/naeem/fcos_pretrained_not_frozen_transformed_1e-4"
+train.init_checkpoint = "detectron2://ImageNetPretrained/MSRA/R-50.pkl"
+train.max_iter = 15000
+train.eval_period = 200
 train.log_period = 10
+train.checkpointer = dict(period=200, max_to_keep=10)
 train.checkpointer = dict(period=200, max_to_keep=10)
 
 def register_dataset():
