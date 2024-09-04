45caefa6175a900cb12646ecb7103a74ae123ad1
Naeem Iqbal <naeemiqbal996@gmail.com>
2024-09-04 14:02:09 +0200

    Phenobench evaluator

--- detectron2/evaluation/__init__.py
+++ detectron2/evaluation/__init__.py
@@ -7,6 +7,7 @@ from .lvis_evaluation import LVISEvaluator
 from .panoptic_evaluation import COCOPanopticEvaluator
 from .pascal_voc_evaluation import PascalVOCDetectionEvaluator
 from .sem_seg_evaluation import SemSegEvaluator
+from .phenobench_evaluation import PhenoBenchEvaluator
 from .testing import print_csv_format, verify_results
 
 __all__ = [k for k in globals().keys() if not k.startswith("_")]
